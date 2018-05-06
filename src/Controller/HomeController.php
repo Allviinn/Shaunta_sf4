e72716d1373757760515c191c233c40bdb0f3b88
Alvin <mujkicalvin1@gmail.com>
2018-05-06 15:15:57 +0200

    Pagehome 'fnie', inclusion des widgets (form de tri, previsu panier ok), que lesinclusion, ces widgets ne foncitonnent pas encore. Le modal pour le détail d'un produit est encore à faire

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,8 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use App\Entity\Categories;
+use App\Entity\Products;
+use App\Entity\Brand;
 
 class HomeController extends Controller
 {
@@ -14,12 +16,16 @@ class HomeController extends Controller
      */
     public function index()
     {
+    	$dct = $this->getDoctrine();
 
-
-    	$cat = $this->getDoctrine()->getRepository(Categories::class)->getCategories();
+    	$cat = $dct->getRepository(Categories::class)->getCategories();
+    	$products = $dct->getRepository(Products::class)->findBy(['featured' => 1]);
+    	$brand = $dct->getRepository(Brand::class)->findAll();
 
         return $this->render('home/index.html.twig', [
             'cat' => $cat,
+            'products' => $products,
+            'brand' => $brand
         ]);
     }
 }
